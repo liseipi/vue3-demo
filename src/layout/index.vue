478ea885388f3abea193da114333387d14083525
oli <oli@qq.com>
2022-10-20 18:00:51 +0800

    dev

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,69 +1,92 @@
 <template>
-  <a-layout id="layout-box">
-    <a-layout-header class="header">
-      <div class="logo">
-        <img src="@/assets/images/logo.svg" alt="logo" width="24" />
+  <a-layout id='layout-box'>
+    <a-layout-header class='header'>
+      <div class='logo'>
+        <img src='@/assets/images/logo.svg' alt='logo' width='24' />
         Admin Platform
       </div>
       <a-menu
-        v-model:selectedKeys="selectedKeys1"
-        theme="dark"
-        mode="horizontal"
+        v-model:selectedKeys='selectedKeys1'
+        theme='dark'
+        mode='horizontal'
         :style="{ lineHeight: '64px', paddingLeft: '24px' }"
+        class='header-menu'
       >
-        <a-menu-item key="1">nav 1</a-menu-item>
-        <a-menu-item key="2">nav 2</a-menu-item>
-        <a-menu-item key="3">nav 3</a-menu-item>
+        <a-menu-item key='1'>nav 1</a-menu-item>
+        <a-menu-item key='2'>nav 2</a-menu-item>
+        <a-menu-item key='3'>nav 3</a-menu-item>
       </a-menu>
+      <div class='user-setting'>
+        <a-dropdown>
+          <a class="ant-dropdown-link" @click.prevent>
+            <a-avatar>
+              <template #icon><UserOutlined /></template>
+            </a-avatar>
+          </a>
+          <template #overlay>
+            <a-menu>
+              <a-menu-item>
+                <a href="javascript:;">1st menu item</a>
+              </a-menu-item>
+              <a-menu-item>
+                <a href="javascript:;">2nd menu item</a>
+              </a-menu-item>
+              <a-menu-item>
+                <a href="javascript:;">3rd menu item</a>
+              </a-menu-item>
+            </a-menu>
+          </template>
+        </a-dropdown>
+      </div>
     </a-layout-header>
     <a-layout>
-      <a-layout-sider width="200" style="background: #fff">
+      <a-layout-sider width='200' style='background: #fff'>
         <a-menu
-          v-model:selectedKeys="selectedKeys2"
-          v-model:openKeys="openKeys"
-          mode="inline"
+          v-model:selectedKeys='selectedKeys2'
+          v-model:openKeys='openKeys'
+          mode='inline'
           :style="{ height: '100%', borderRight: 0 }"
         >
-          <a-sub-menu key="sub1">
+          <a-sub-menu key='sub1'>
             <template #title>
               <span>
                 <user-outlined />
                 subnav 1
               </span>
             </template>
-            <a-menu-item key="1">option1</a-menu-item>
-            <a-menu-item key="2">option2</a-menu-item>
-            <a-menu-item key="3">option3</a-menu-item>
-            <a-menu-item key="4">option4</a-menu-item>
+            <a-menu-item key='1'>option1</a-menu-item>
+            <a-menu-item key='2'>option2</a-menu-item>
+            <a-menu-item key='3'>option3</a-menu-item>
+            <a-menu-item key='4'>option4</a-menu-item>
           </a-sub-menu>
-          <a-sub-menu key="sub2">
+          <a-sub-menu key='sub2'>
             <template #title>
               <span>
                 <laptop-outlined />
                 subnav 2
               </span>
             </template>
-            <a-menu-item key="5">option5</a-menu-item>
-            <a-menu-item key="6">option6</a-menu-item>
-            <a-menu-item key="7">option7</a-menu-item>
-            <a-menu-item key="8">option8</a-menu-item>
+            <a-menu-item key='5'>option5</a-menu-item>
+            <a-menu-item key='6'>option6</a-menu-item>
+            <a-menu-item key='7'>option7</a-menu-item>
+            <a-menu-item key='8'>option8</a-menu-item>
           </a-sub-menu>
-          <a-sub-menu key="sub3">
+          <a-sub-menu key='sub3'>
             <template #title>
               <span>
                 <notification-outlined />
                 subnav 3
               </span>
             </template>
-            <a-menu-item key="9">option9</a-menu-item>
-            <a-menu-item key="10">option10</a-menu-item>
-            <a-menu-item key="11">option11</a-menu-item>
-            <a-menu-item key="12">option12</a-menu-item>
+            <a-menu-item key='9'>option9</a-menu-item>
+            <a-menu-item key='10'>option10</a-menu-item>
+            <a-menu-item key='11'>option11</a-menu-item>
+            <a-menu-item key='12'>option12</a-menu-item>
           </a-sub-menu>
         </a-menu>
       </a-layout-sider>
-      <a-layout style="padding: 0 24px 24px">
-        <a-breadcrumb style="margin: 16px 0">
+      <a-layout style='padding: 0 24px 24px'>
+        <a-breadcrumb style='margin: 16px 0'>
           <a-breadcrumb-item>Home</a-breadcrumb-item>
           <a-breadcrumb-item>List</a-breadcrumb-item>
           <a-breadcrumb-item>App</a-breadcrumb-item>
@@ -76,7 +99,7 @@
             minHeight: '280px'
           }"
         >
-          <a-spin :spinning="storeLoading.loadingState" :delay="300">
+          <a-spin :spinning='storeLoading.loadingState' :delay='300'>
             <router-view />
           </a-spin>
         </a-layout-content>
@@ -84,7 +107,7 @@
     </a-layout>
   </a-layout>
 </template>
-<script lang="ts" setup>
+<script lang='ts' setup>
 import { ref } from 'vue'
 import { useLoadingStore } from '@/store'
 
@@ -95,22 +118,35 @@ const selectedKeys2 = ref<string[]>(['1'])
 const collapsed = ref<boolean>(false)
 const openKeys = ref<string[]>(['sub1'])
 </script>
-<style lang="scss" scoped>
+<style lang='scss' scoped>
 #layout-box {
   height: 100vh;
+
+  .header {
+    padding: 0;
+    display: flex;
+  }
+
+  .logo {
+    transition: all 0.3s;
+    color: #fff;
+    font-size: 18px;
+    font-family: Avenir, Helvetica Neue, Arial, Helvetica, sans-serif;
+    font-weight: 600;
+    vertical-align: middle;
+    width: 200px;
+    text-align: center;
+  }
+
+  .header-menu {
+    flex-grow: 1;
+  }
+
+  .user-setting {
+    color: #fff;
+    line-height: 64px;
+  }
 }
-.ant-layout-header {
-  padding: 0;
-}
-.logo {
-  transition: all 0.3s;
-  color: #fff;
-  font-size: 18px;
-  font-family: Avenir, Helvetica Neue, Arial, Helvetica, sans-serif;
-  font-weight: 600;
-  vertical-align: middle;
-  width: 200px;
-  text-align: center;
-  float: left;
-}
+
+
 </style>
